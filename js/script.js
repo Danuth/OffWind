3d0300dd310d9f250a532e98a53fd7757c72ca33
Duong Pechdanuth <2020069duong@aupp.edu.kh>
2022-04-20 13:59:35 +0700

    3rd commit

--- js/script.js
+++ js/script.js
@@ -14,19 +14,21 @@ window.addEventListener('scroll', () =>
     }
 });
 
+// show navbar
 navbarShowBtn.addEventListener('click', () =>
 {
     navbarCollapseDiv.classList.add('navbar-collapse-rmw');
 });
 
+// close navbar
 navbarCloseBtn.addEventListener('click', () =>
 {
     navbarCollapseDiv.classList.remove('navbar-collapse-rmw');
 });
 
-document.addEventListener('click', (e) =>
+document.addEventListener('click', (e) => 
 {
-    if(e.target.id != "navbar-collapse" && e.target.id != "navbar-show-btn" && e.parentElement.id != "navbar-show-btn")
+    if(e.target.id != "navbar-collapse" && e.target.id != "navbar-show-btn" && e.target.parentElement.id != "navbar-show-btn")
     {
         navbarCollapseDiv.classList.remove('navbar-collapse-rmw');
     }
