8c14bf2d12e02007fdb9d4e89f10db7f0aa1436f
PichsoL <2020050long@aupp.edu.kh>
2022-04-24 14:14:01 +0700

    14th commit

--- js/script.js
+++ js/script.js
@@ -2,56 +2,46 @@ let navbarDiv = document.querySelector('.navbar');
 const navbarCollapseDiv = document.getElementById('navbar-collapse');
 const navbarShowBtn = document.getElementById('navbar-show-btn');
 const navbarCloseBtn = document.getElementById('navbar-close-btn');
-window.addEventListener('scroll', () =>
-{
-    if(document.body.scrollTop > 40 || document.documentElement.scrollTop > 40)
-    {
+window.addEventListener('scroll', () => {
+    if (document.body.scrollTop > 40 || document.documentElement.scrollTop > 40) {
         navbarDiv.classList.add('navbar-cng');
     }
-    else
-    {
+    else {
         navbarDiv.classList.remove('navbar-cng');
     }
 });
 
 // show navbar
-navbarShowBtn.addEventListener('click', () =>
-{
+navbarShowBtn.addEventListener('click', () => {
     navbarCollapseDiv.classList.add('navbar-collapse-panel');
 });
 
 // close navbar
-navbarCloseBtn.addEventListener('click', () =>
-{
+navbarCloseBtn.addEventListener('click', () => {
     navbarCollapseDiv.classList.remove('navbar-collapse-panel');
 });
 
-document.addEventListener('click', (e) => 
-{
-    if(e.target.id != "navbar-collapse" && e.target.id != "navbar-show-btn" && e.target.parentElement.id != "navbar-show-btn")
-    {
+document.addEventListener('click', (e) => {
+    if (e.target.id != "navbar-collapse" && e.target.id != "navbar-show-btn" && e.target.parentElement.id != "navbar-show-btn") {
         navbarCollapseDiv.classList.remove('navbar-collapse-panel');
     }
 });
 
-function scrollUp()
-{
+function scrollUp() {
     const scrollUp = document.getElementById('scroll-up');
-    
+
     // scroll-up visible when the scroll is higher than 750vh (after the header)
-    if(this.scrollY >= 750) 
-    {
+    if (this.scrollY >= 750) {
         scrollUp.classList.add('show-scroll');
-    }   
-    else 
-    {
+    }
+    else {
         scrollUp.classList.remove('show-scroll')
     }
 }
 window.addEventListener('scroll', scrollUp)
 
 var darkMode = document.getElementById('mode-btn');
-darkMode.onclick = function()
-{
+darkMode.onclick = function () {
     document.body.classList.toggle('dark-mode');
 }
+
