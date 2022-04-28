d8bc3d0ca639b0a6300262423e551f4988102178
Duong Pechdanuth <2020069duong@aupp.edu.kh>
2022-04-28 18:06:58 +0700

    37th commit

--- js/script.js
+++ js/script.js
@@ -78,3 +78,16 @@ closePopup.addEventListener('click', () => {
     popup.classList.remove('open-popup');
 })
 
+// login
+
+let loginForm = document.querySelector('.login-form-container');
+
+document.querySelector('#login-btn').onclick = () =>
+{
+    loginForm.classList.toggle('active');
+}
+
+document.querySelector('#close-login-btn').onclick = () =>
+{
+    loginForm.classList.remove('active');
+}
