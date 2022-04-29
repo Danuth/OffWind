f556ce76080d898c22bf5e3874c13256793d0187
Duong Pechdanuth <2020069duong@aupp.edu.kh>
2022-04-30 00:09:45 +0700

    42nd commit

--- js/package.js
+++ js/package.js
@@ -25,6 +25,56 @@ function load() {
 
 load();
 
+function updateBookingTotal() {
+    var bookingItemContainer = document.getElementsByClassName('booking-items')[0];
+    var bookingRows = bookingItemContainer.getElementsByClassName('booking-row');
+    var total = 0;
+
+    for (var i = 0; i < bookingRows.length; i++) {
+        var bookingRow = bookingRows[i];
+        var cost = bookingRow.getElementsByClassName('booking-price')[0];
+        var pax = bookingRow.getElementsByClassName('booking-pax-input')[0];
+        var price = parseFloat(cost.innerText.replace('$', ''));
+        var quantity = pax.value;
+
+        total += (price * quantity);
+    }
+
+    total = Math.round(total * 100) / 100;
+    document.getElementsByClassName('booking-total-price')[0].innerText = '$' + total;
+}
+
+function addItemToList(title, price, imageSrc) {
+    var bookingRow = document.createElement('div');
+    bookingRow.classList.add('booking-row');
+
+    var bookingItem = document.getElementsByClassName('booking-items')[0];
+    var bookingItems = bookingItem.getElementsByClassName('booking-item-title');
+
+    for (var i = 0; i < bookingItems.length; i++) {
+        if (bookingItems[i].innerText == title) {
+            alert('This package has already been added to the list.');
+            return;
+        }
+    }
+
+    var newBookingRow = `
+        <div class="booking-item booking-column">
+            <img class="booking-item-image" src="${imageSrc}" width="100" height="100">
+            <span class="booking-item-title">${title}</span>
+        </div>
+        <span class="booking-price booking-column">${price}</span>
+        <div class="booking-pax booking-column">
+            <input class="booking-pax-input" type="number" value="1">
+            <button class="btn btn-remove" type="button">REMOVE</button>
+        </div>`;
+
+    bookingRow.innerHTML = newBookingRow;
+    bookingItem.append(bookingRow);
+    bookingRow.getElementsByClassName('btn-remove')[0].addEventListener('click', removeBookingItem);
+    bookingRow.getElementsByClassName('booking-pax-input')[0].addEventListener('change', paxUpdated);
+}
+
 function bookingNow() {
     alert('Thank you for booking with us!');
     var bookingItems = document.getElementsByClassName('booking-items')[0];
@@ -64,52 +114,5 @@ function addTo(event) {
     updateBookingTotal();
 }
 
-function addItemToList(title, price, imageSrc) {
-    var bookingRow = document.createElement('div');
-    bookingRow.classList.add('booking-row');
 
-    var bookingItem = document.getElementsByClassName('booking-items')[0];
-    var bookingItems = bookingItem.getElementsByClassName('booking-item-title');
 
-    for (var i = 0; i < bookingItems.length; i++) {
-        if (bookingItems[i].innerText == title) {
-            alert('This package has already been added to the list.');
-            return;
-        }
-    }
-
-    var bookingRowContents = `
-        <div class="booking-item booking-column">
-            <img class="booking-item-image" src="${imageSrc}" width="100" height="100">
-            <span class="booking-item-title">${title}</span>
-        </div>
-        <span class="booking-price booking-column">${price}</span>
-        <div class="booking-pax booking-column">
-            <input class="booking-pax-input" type="number" value="1">
-            <button class="btn btn-remove" type="button">REMOVE</button>
-        </div>`;
-
-    bookingRow.innerHTML = bookingRowContents;
-    bookingItem.append(bookingRow);
-    bookingRow.getElementsByClassName('btn-remove')[0].addEventListener('click', removeBookingItem);
-    bookingRow.getElementsByClassName('booking-pax-input')[0].addEventListener('change', paxUpdated);
-}
-
-function updateBookingTotal() {
-    var bookingItemContainer = document.getElementsByClassName('booking-items')[0];
-    var bookingRows = bookingItemContainer.getElementsByClassName('booking-row');
-    var total = 0;
-
-    for (var i = 0; i < bookingRows.length; i++) {
-        var bookingRow = bookingRows[i];
-        var cost = bookingRow.getElementsByClassName('booking-price')[0];
-        var pax = bookingRow.getElementsByClassName('booking-pax-input')[0];
-        var price = parseFloat(cost.innerText.replace('$', ''));
-        var quantity = pax.value;
-
-        total += (price * quantity);
-    }
-
-    total = Math.round(total * 100) / 100;
-    document.getElementsByClassName('booking-total-price')[0].innerText = '$' + total;
-}
